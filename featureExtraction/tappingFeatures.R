4800a0215733f1606f11c717e5b801b3d021d871
Abhi <abhishek.vit@gmail.com>
2018-10-31 10:24:11 -0700

    first version mpowertools to mhealthtools conversion

--- featureExtraction/tappingFeatures.R
+++ featureExtraction/tappingFeatures.R
@@ -1,33 +1,28 @@
 # elevateMS tapping feature extraction
-
+rm(list=ls())
 # load all necessary libraries
-library(synapseClient)
-library(plyr)
-library(dplyr)
-library(ggplot2)
-library(doMC)
-library(jsonlite)
-library(parallel)
-library(tidyr)
-library(lubridate)
-library(stringr)
-library(sqldf)
-library(parsedate)
+library(install.load)
+install_load(c('plyr', 'dplyr', 'doMC', 'jsonlite', 'parallel', 'tidyr', 'lubridate'))
+install_load(c('stringr', 'sqldf', 'parsedate', 'synapser'))
 library(githubr) # to install githubr run: library(devtools); devtools::install_github("brian-bot/githubr")
-library(mpowertools) # to install mpowertools run: library(devtools); devtools::install_github("Sage-Bionetworks/mpowertools")
+# to install mpowertools run: 
+#library(devtools)
+devtools::install_github("Sage-Bionetworks/mhealthtools")
+install_load("mhealthtools")
+
 
 # login to Synapse
-synapseLogin()
+synLogin()
 
 # Query table of interest, tapping activity v2
 INPUT_TAPPING_ACTIVITY_TABLE_SYNID = 'syn10278765'
 actv_tapping_syntable <- synTableQuery(paste0("SELECT * FROM ", INPUT_TAPPING_ACTIVITY_TABLE_SYNID))
-actv_tapping <- actv_tapping_syntable@values
+actv_tapping <- actv_tapping_syntable$asDataFrame()
 actv_tapping$idx <- rownames(actv_tapping)
 
 # timestamp shenanigans
-actv_tapping$createdOn <- as.POSIXct(actv_tapping$createdOn, tz="UTC")
-actv_tapping$createdOn <- format_iso_8601(actv_tapping$createdOn)
+actv_tapping$createdOn <- lubridate::as_datetime(actv_tapping$createdOn/1000)
+#actv_tapping$createdOn <- format_iso_8601(actv_tapping$createdOn)
 
 # save for later
 selected_records <- actv_tapping$recordId
@@ -70,15 +65,77 @@ if (detectCores() >= 2) {
 }
 registerDoMC(detectCores() - 2)
 
+
+readTappingFile <- function(tappingJsonFile) {
+  tryCatch({
+    tapData <- jsonlite::fromJSON(tappingJsonFile)
+    list(data = tapData, error = FALSE)
+  }, error = function(err) {
+    list(data = NA, error = TRUE)
+  })
+}
+
+shapeTappingData <- function(tapData) {
+  tapData <- dplyr::mutate(tapData, TapCoordinate = stringr::str_replace_all(TapCoordinate,"[{}}]", "")) %>%
+    tidyr::separate(TapCoordinate,into = c("X", "Y"), sep = ",") %>%
+    dplyr::mutate(x = as.numeric(X),y = as.numeric(Y)) %>%
+    dplyr::mutate(t = TapTimeStamp,buttonid = TappedButtonId) %>%
+    dplyr::select(t,x, y, buttonid)
+  return(tapData)
+}
+
+createTappingFeaturesErrorResult <- function(error) {
+  df <- data.frame(t(c(rep(NA, 42), error)))
+  colnames(df) <- c("meanTapInter", "medianTapInter",
+                    "iqrTapInter", "minTapInter", "maxTapInter",
+                    "skewTapInter", "kurTapInter", "sdTapInter",
+                    "madTapInter", "cvTapInter", "rangeTapInter",
+                    "tkeoTapInter", "ar1TapInter", "ar2TapInter",
+                    "fatigue10TapInter", "fatigue25TapInter",
+                    "fatigue50TapInter", "meanDriftLeft",
+                    "medianDriftLeft", "iqrDriftLeft", "minDriftLeft",
+                    "maxDriftLeft", "skewDriftLeft", "kurDriftLeft",
+                    "sdDriftLeft", "madDriftLeft", "cvDriftLeft",
+                    "rangeDriftLeft", "meanDriftRight",
+                    "medianDriftRight", "iqrDriftRight",
+                    "minDriftRight", "maxDriftRight", "skewDriftRight",
+                    "kurDriftRight", "sdDriftRight", "madDriftRight",
+                    "cvDriftRight", "rangeDriftRight", "numberTaps",
+                    "buttonNoneFreq", "corXY", "error")
+  df
+}
+
+
+getTappingFeatures <- function(tappingJsonFile, depressThr=20, removeDups=T) {
+  tapData <- readTappingFile(tappingJsonFile)
+  error <- tapData$error
+  tapData <- tapData$data
+  if (error == T) {
+    print(tappingJsonFile)
+    tapFeatures <- createTappingFeaturesErrorResult("unable to read JSON file")
+  } else if (is.data.frame(tapData) == F) {
+    print(tappingJsonFile)
+    tapFeatures <- createTappingFeaturesErrorResult("expected data frame object after reading tapping json file")
+  } else if (nrow(tapData) < 5) {
+    print(tappingJsonFile)
+    tapFeatures <- createTappingFeaturesErrorResult("raw tapping data has less than 5 rows")
+  } else {
+    # shape data
+    tapData <- shapeTappingData(tapData)
+    mhealthtools::get_tapping_features(tapData)
+  }
+}
+
 # extract LEFT hand tapping features
 left_hand_tapping_features <-
   ddply(
     .data = actv_tapping,
     .variables = colnames(actv_tapping),
     .parallel = T,
-    .fun = function(row)
-      mpowertools::getTappingFeatures(row$tapping_left_json_file)
-  )
+    .fun = function(row) {
+      getTappingFeatures(row$tapping_right_json_file)
+    }
+)
 
 left_hand_tapping_features <-
   left_hand_tapping_features %>% mutate(hand = 'left')
@@ -90,7 +147,7 @@ right_hand_tapping_features <-
     .variables = colnames(actv_tapping),
     .parallel = T,
     .fun = function(row) {
-      mpowertools::getTappingFeatures(row$tapping_right_json_file)
+      getTappingFeatures(row$tapping_right_json_file)
     }
   ) %>%
   mutate(hand = 'right')
@@ -98,8 +155,10 @@ right_hand_tapping_features <-
 tappingFeatures <-
   rbind(left_hand_tapping_features, right_hand_tapping_features)
 
+setdiff(colnames(left_hand_tapping_features), colnames(right_hand_tapping_features))
+
 # View the data
-# View(tappingFeatures)
+View(tappingFeatures)
 
 #############
 # Final Data
@@ -127,5 +186,5 @@ synStore(File(OUTPUT_FILE, parentId=OUTPUT_FOLDER_ID),
          activityName = activityName,
          activityDescription = activityDescription,
          used = INPUT_TAPPING_ACTIVITY_TABLE_SYNID,
-         executed = list(thisFile, "https://github.com/Sage-Bionetworks/mpowertools"))
+         executed = list(thisFile, "https://github.com/Sage-Bionetworks/mhealthtools"))
 unlink(OUTPUT_FILE)
