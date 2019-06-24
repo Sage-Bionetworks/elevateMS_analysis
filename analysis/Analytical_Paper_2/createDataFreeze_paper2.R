a6df325bdfd92ce6947d16ad7fe1b54be0e83856
Abhi <abhishek.vit@gmail.com>
2019-06-24 19:31:50 +0000

    added NeuroQoLs weekly avg and weather features

--- analysis/Analytical_Paper_2/createDataFreeze_paper2.R
+++ analysis/Analytical_Paper_2/createDataFreeze_paper2.R
@@ -11,19 +11,28 @@ WEEKS_SINCE_LAUNCH = ( (DAYS_SINCE_LAUNCH - 1) %/% 7) + 1
 #baselineChar - no need to filter
 
 userActivity <- userActivity %>% filter(createdOn_localTime <= FREEZE_DATE)
-triggers <- triggers %>% filter(activity_start_timestamp_local <= FREEZE_DATE & participant_week <= 12)
-relapses <- relapses %>% filter(activity_start_timestamp_local <= FREEZE_DATE & participant_week <= 12)
-dsst <- dsst %>% filter(activtyStartTime_GMT <= FREEZE_DATE & participant_week <= 12)
-symptoms <- symptoms %>% filter(activity_start_timestamp_local <= FREEZE_DATE & participant_week <= 12)
-dailyCheckins <-  dailyCheckins %>% filter(activity_start_timestamp_local <= FREEZE_DATE & participant_week <= 12)
-tapF <- tapF %>% filter(activity_start_timestamp_local <= FREEZE_DATE & participant_week <= 12)
-nQOL_uppExtremity <- nQOL_uppExtremity  %>% filter(activity_start_timestamp_local <= FREEZE_DATE & participant_week <= 12)
-nQOL_lowExtremity  <- nQOL_lowExtremity %>% filter(activity_start_timestamp_local <= FREEZE_DATE & participant_week <= 12)
-nQOL_cognition  <- nQOL_cognition %>% filter(activity_start_timestamp_local <= FREEZE_DATE & participant_week <= 12)
-walkF <- walkF  %>% filter(activity_start_timestamp_local <= FREEZE_DATE & participant_week <= 12)
-restF <- restF %>% filter(activity_start_timestamp_local <= FREEZE_DATE & participant_week <= 12)
+triggers <- triggers %>% filter(activity_start_timestamp_local <= FREEZE_DATE)
+relapses <- relapses %>% filter(activity_start_timestamp_local <= FREEZE_DATE)
+dsst <- dsst %>% filter(activtyStartTime_GMT <= FREEZE_DATE)
+symptoms <- symptoms %>% filter(activity_start_timestamp_local <= FREEZE_DATE)
+dailyCheckins <-  dailyCheckins %>% filter(activity_start_timestamp_local <= FREEZE_DATE )
+tapF <- tapF %>% filter(activity_start_timestamp_local <= FREEZE_DATE)
+nQOL_uppExtremity <- nQOL_uppExtremity  %>% filter(activity_start_timestamp_local <= FREEZE_DATE)
+nQOL_lowExtremity  <- nQOL_lowExtremity %>% filter(activity_start_timestamp_local <= FREEZE_DATE)
+nQOL_cognition  <- nQOL_cognition %>% filter(activity_start_timestamp_local <= FREEZE_DATE )
+walkF <- walkF  %>% filter(activity_start_timestamp_local <= FREEZE_DATE)
+restF <- restF %>% filter(activity_start_timestamp_local <= FREEZE_DATE)
+weatherF <- weatherF %>% filter(activity_start_timestamp_local <= FREEZE_DATE)
 
-needed <- c('triggers', 'dailyCheckins', 'relapses',
+#nQOL_uppExtremity_week_avg
+
+#nQOL_lowExtremity_week_avg
+
+#nQOL_cognition_week_avg
+
+needed <- c('triggers', 'dailyCheckins', 'relapses', 'weatherF',
+            'nQOL_uppExtremity_week_avg', 'nQOL_lowExtremity_week_avg',
+            'nQOL_cognition_week_avg',
             'symptoms', 'nQOL_uppExtremity', 
             'nQOL_lowExtremity', 'dsst','nQOL_cognition',
             'tapF', 'walkF', 'restF', 'userActivity', 
