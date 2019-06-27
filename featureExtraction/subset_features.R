42f631a63499eb827f25cfc8883d622a5ff4f5ef
itismeghasyam <rstudio@ip-10-5-26-136.ec2.internal>
2019-06-27 19:39:57 +0000

    - Changed the subset feature file to run on the latest features

--- featureExtraction/subset_features.R
+++ featureExtraction/subset_features.R
@@ -1,49 +1,88 @@
-# Subsetting elevateMS features to the most important features in mPower data
-rm(list = ls())
-gc()
+############################################################################
+# ElevateMS project
+# Purpose: Subset the summarized features to selected features (using mPower feature ranking)
+# Author: Meghasyam Tummalacherla
+############################################################################
 
-## tremor feature subsetting
+###########################################################
+## Required libraries
+###########################################################
 library(tidyverse)
 library(synapser)
 library(githubr)
 synapser::synLogin()
 
-## Downlaod the important features from mpower analysis
-mpower_features <- synapser::synGet('syn17088603')$path %>% read.csv(sep = '\t')
-all.used.ids <- 'syn17088603'
+###########################################################
+## Download and set up features for elevateMS
+###########################################################
+summarized.ftrs.elevateMS.id = 'syn18879962'
+summarized.ftrs.elevateMS = read.csv(synapser::synGet(summarized.ftrs.elevateMS.id)$path,
+                                     sep = '\t')
+summarized.ftrs.elevateMS.id.record = 'syn19964391'
+summarized.ftrs.elevateMS.record = read.csv(synapser::synGet(summarized.ftrs.elevateMS.id.record)$path,
+                                            sep = '\t')
+all.used.ids = c(summarized.ftrs.elevateMS.id, summarized.ftrs.elevateMS.id.record)
 
-## Get kinetic features (hand to nose test)
-mpower_features <- mpower_features %>% dplyr::filter(assay == 'kinetic')
+###########################################################
+## Download and set up features for age matched mpower controls
+###########################################################
+summarized.ftrs.mpower.id = 'syn19165094'
+summarized.ftrs.mpower = read.csv(synapser::synGet(summarized.ftrs.mpower.id)$path,
+                                  sep = '\t')
+summarized.ftrs.mpower.id.record = 'syn19964696'
+summarized.ftrs.mpower.record = read.csv(synapser::synGet(summarized.ftrs.mpower.id.record)$path,
+                                         sep = '\t')
+all.used.ids <- c(all.used.ids, summarized.ftrs.mpower.id, summarized.ftrs.mpower.id.record)
 
-## Download all the elevateMS features
-tremor_features_left <- synapser::synGet('syn12104398')$path %>% read.csv(sep='\t')
-tremor_features_right <- synapser::synGet('syn12104396')$path %>% read.csv(sep='\t')
-all.used.ids <- c(all.used.ids, 'syn12104398', 'syn12104396')
+##########################################################
+## Download feature ranking and subset features
+###########################################################
+ranking.syn.id = 'syn12038526' # AllRanking.Rdata (feature ranking  postural, kinetic and rest for mpower)
+load(synapser::synGet(ranking.syn.id)$path)
+all.used.ids <- c(all.used.ids, ranking.syn.id)
 
-## Get the covariate columns
-feature_list <- c(grep('.fr', colnames(tremor_features_left)),
-                  grep('.tm', colnames(tremor_features_left)),
-                  grep('EnergyInBand', colnames(tremor_features_left)))
+# find important features
+nFeatures = 100 # max number of features to choose per assay
+kineticFeaturesIMF1 <- ranka[,3] %>% na.omit() 
+kineticFeaturesIMF2 <- ranka[,4] %>% na.omit() 
+maxRank <- max(c(kineticFeaturesIMF1,kineticFeaturesIMF2))
+kineticFeaturesIMF1 <- kineticFeaturesIMF1[kineticFeaturesIMF1 > (maxRank-nFeatures)] %>% names()
+kineticFeaturesIMF2 <- kineticFeaturesIMF2[kineticFeaturesIMF2 > (maxRank-nFeatures)] %>% names()
 
-covariate_list <- setdiff(colnames(tremor_features_left),
-                          colnames(tremor_features_left)[feature_list])
+kineticFeaturesToSelect <- c()
+for (ftr in colnames(summarized.ftrs.elevateMS)){
+  ftr1 <- gsub('IMF1.','',ftr)
+  # print(ftr1)
+  if(ftr1 %in% kineticFeaturesIMF1){
+    kineticFeaturesToSelect <- c(kineticFeaturesToSelect,ftr)
+  }
+  ftr1 <- gsub('IMF2.','',ftr)
+  # print(ftr1)
+  if(ftr1 %in% kineticFeaturesIMF2){
+    kineticFeaturesToSelect <- c(kineticFeaturesToSelect,ftr)
+  }
+}
 
-subsetted_feature_list <- mpower_features$Feature %>% droplevels() 
+# Subset features
+hc.summarized.features <- dplyr::full_join(summarized.ftrs.elevateMS, summarized.ftrs.mpower) %>% 
+  dplyr::select(c('healthCode',kineticFeaturesToSelect)) %>% 
+  unique()
 
-## Subsetted feature and covariate set
-tremor_features_left_subset <- tremor_features_left[ ,subsetted_feature_list]
-tremor_features_left_subset <- tremor_features_left_subset %>% 
-  cbind(tremor_features_left$recordId)
+recordId.summarized.features <- dplyr::full_join(summarized.ftrs.elevateMS.record, summarized.ftrs.mpower.record) %>% 
+  dplyr::select(c('healthCode',kineticFeaturesToSelect)) %>% 
+  unique()
 
-tremor_features_right_subset <- tremor_features_right[, subsetted_feature_list]
-tremor_features_right_subset <- tremor_features_right_subset %>% 
-  cbind(tremor_features_right$recordId)
+#############
+# Upload data to Synapse
+#############
+# upload file to Synapse with provenance
+# to learn more about provenance in Synapse, go to http://docs.synapse.org/articles/provenance.html
 
 ## Github link
 # Copy paste the github token string and store it as 'github_token.txt' file
 # A github token is required to access the elevateMS_analysis repository as it is private
-gtToken = 'github_token.txt'
-githubr::setGithubToken(as.character(read.table(gtToken)$V1))
+gtToken = '~/github_token.txt'
+setGithubToken(as.character(read.table(gtToken)$V1))
 thisFileName <- "featureExtraction/subset_features.R" # location of file inside github repo
 thisRepo <- getRepo(repository = "itismeghasyam/elevateMS_analysis", 
                     ref="branch", 
@@ -51,13 +90,13 @@ thisRepo <- getRepo(repository = "itismeghasyam/elevateMS_analysis",
 thisFile <- getPermlink(repository = thisRepo, repositoryPath=thisFileName)
 
 # name and describe this activity
-activityName = "Subset tremor features"
-activityDescription = "Subset best tremor features from mPower Analysis"
+activityName = "Subset to important features"
+activityDescription = "Subset the whole feature file into a smaller subset using feature ranking from mPower"
 
-# upload to Synapse, left hand features
-synapse.folder.id <- "syn10140063" # synId of folder to upload your file to
-OUTPUT_FILE <- "tremorFeatures_handToNoseLeft_subset.tsv" # name your file
-write.table(tremor_features_left_subset, OUTPUT_FILE, sep="\t", row.names=F, quote=F, na="")
+# upload to Synapse, healthCode wise summarized features
+synapse.folder.id <- "syn19963670" # synId of folder to upload your file to
+OUTPUT_FILE <- "final_subset_features_healthCode.tsv" # name your file
+write.table(hc.summarized.features, OUTPUT_FILE, sep="\t", row.names=F, quote=F, na="")
 synStore(File(OUTPUT_FILE, parentId=synapse.folder.id),
          activityName = activityName,
          activityDescription = activityDescription,
@@ -65,13 +104,14 @@ synStore(File(OUTPUT_FILE, parentId=synapse.folder.id),
          executed = thisFile)
 unlink(OUTPUT_FILE)
 
-# upload to Synapse, right hand features
-synapse.folder.id <- "syn10140063" # synId of folder to upload your file to
-OUTPUT_FILE <- "tremorFeatures_handToNoseRight_subset.tsv" # name your file
-write.table(tremor_features_right_subset, OUTPUT_FILE, sep="\t", row.names=F, quote=F, na="")
+# upload to Synapse, recordwise summarized features
+synapse.folder.id <- "syn19963670" # synId of folder to upload your file to
+OUTPUT_FILE <- "final_subset_features_recordId.tsv" # name your file
+write.table(recordId.summarized.features, OUTPUT_FILE, sep="\t", row.names=F, quote=F, na="")
 synStore(File(OUTPUT_FILE, parentId=synapse.folder.id),
          activityName = activityName,
          activityDescription = activityDescription,
          used = all.used.ids,
          executed = thisFile)
 unlink(OUTPUT_FILE)
+
