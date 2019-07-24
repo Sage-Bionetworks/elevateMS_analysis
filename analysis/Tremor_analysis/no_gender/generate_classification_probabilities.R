8245b9dfca20fe522ef287120d73694d1a18c477
itismeghasyam <rstudio@ip-10-5-26-136.ec2.internal>
2019-07-24 21:46:11 +0000

    classification probabilities now generated for each record (based on model built using healthCode wise summarized features)

--- analysis/Tremor_analysis/no_gender/generate_classification_probabilities.R
+++ analysis/Tremor_analysis/no_gender/generate_classification_probabilities.R
@@ -51,7 +51,7 @@ dat.train <- summarized.ftrs.healthCode %>%
 ftrs.to.evaluate <- setdiff(colnames(summarized.ftrs.healthCode), 'healthCode')
 
 ## Test data
-dat.test <- summarized.ftrs.healthCode %>%
+dat.test.hc <- summarized.ftrs.healthCode %>%
   # dplyr::filter(!(healthCode %in% age.records.matched$healthCode)) %>%
   dplyr::left_join(demo.tbl.ms %>%
                      dplyr::select(healthCode, gender, age, MS = dataGroups)) %>%
@@ -63,33 +63,81 @@ dat.test <- summarized.ftrs.healthCode %>%
   unique() %>% 
   droplevels()
 
+dat.test.record <- summarized.ftrs.record %>%
+  # dplyr::filter(!(healthCode %in% age.records.matched$healthCode)) %>%
+  dplyr::left_join(demo.tbl.ms %>%
+                     dplyr::select(healthCode, gender, age, MS = dataGroups)) %>%
+  dplyr::filter(MS %in% c('ms_patient','control'),
+                gender %in% c('male','female'),
+                !is.na(age), !is.na(MS), !is.na(gender)) %>% #remove NAs
+  dplyr::mutate(MS = factor(MS, levels = c('control','ms_patient')),
+                MS = as.numeric(MS) - 1) %>% 
+  unique() %>% 
+  droplevels()
 
 #### CASE - I (RF forest predicts classes for all 1000 trees i.e building a
 #              classification forest/tree)
+####
+
 # Build rf model using train data
 rf.mdl = getRfModel(dat.train, responseNames = 'MS')
-rf.prediction = predict(rf.mdl, 
-                        data = dat.test,
+
+# Predict probabilities for each healthCode
+rf.prediction.hc = predict(rf.mdl, 
+                        data = dat.test.hc,
                         predict.all = T,
                         type = 'response',
                         seed = 123456)
-pred.probabilities.1 = rowSums(rf.prediction$predictions == max(dat.test$MS), na.rm = T)/rf.prediction$num.trees
+pred.probabilities.1.hc = rowSums(rf.prediction.hc$predictions == max(dat.test.hc$MS), na.rm = T)/rf.prediction.hc$num.trees
+
+# Predict probabilities for each record
+rf.prediction.record = predict(rf.mdl, 
+                        data = dat.test.record,
+                        predict.all = T,
+                        type = 'response',
+                        seed = 123456)
+pred.probabilities.1.record = rowSums(rf.prediction.record$predictions == max(dat.test.record$MS), na.rm = T)/rf.prediction.record$num.trees
 
 #### CASE - II (RF forest predicts a single probability using all 1000 trees, 
 #               i.e the tree type is probability estimation)
+####
+
 # Build rf model using train data
 rf.mdl = getRfModel(dat.train, responseNames = 'MS', prob = TRUE)
-rf.prediction = predict(rf.mdl, 
-                        data = dat.test,
+
+# Predict probabilities for each healthCode
+rf.prediction.hc = predict(rf.mdl, 
+                        data = dat.test.hc,
                         predict.all = F,
                         type = 'response',
                         seed = 123456)
-pred.probabilities.2 = rf.prediction$predictions[,1]
+pred.probabilities.2.hc = rf.prediction.hc$predictions[,1]
 
-pred.prob <- data.frame(healthCode = dat.test$healthCode, 
-                        MS = dat.test$MS,
-                        prob_classification = pred.probabilities.1,
-                        prob_probability_estimation = pred.probabilities.2)
+# Predict probabilities for each record
+rf.prediction.record = predict(rf.mdl, 
+                           data = dat.test.record,
+                           predict.all = F,
+                           type = 'response',
+                           seed = 123456)
+pred.probabilities.2.record= rf.prediction.record$predictions[,1]
+
+
+pred.prob.hc <- data.frame(healthCode = dat.test.hc$healthCode, 
+                        MS = dat.test.hc$MS,
+                        prob_classification = pred.probabilities.1.hc,
+                        prob_probability_estimation = pred.probabilities.2.hc)
+
+pred.prob.record <- data.frame(healthCode = dat.test.record$healthCode,
+                               recordId = dat.test.record$recordId,
+                               MS = dat.test.record$MS,
+                               Assay = dat.test.record$Assay,
+                               prob_classification = pred.probabilities.1.record,
+                               prob_probability_estimation = pred.probabilities.2.record) %>% 
+  dplyr::left_join(summarized.ftrs.record %>% 
+                     dplyr::select(
+                       setdiff(colnames(summarized.ftrs.record), colnames(summarized.ftrs.healthCode))
+                     )) %>% 
+  unique()
 
 ###########################################################
 ## Github Parameters and Synapse
@@ -110,8 +158,19 @@ thisFile <- getPermlink(repository = thisRepo, repositoryPath=paste0('analysis/T
 
 # upload to Synapse, prediction probabilities
 synapse.folder.id <- parentId # synId of folder to upload your file to
-OUTPUT_FILE <- "predition_probabilities.tsv" # name your file
-write.table(pred.prob, OUTPUT_FILE, sep="\t", row.names=F, quote=F, na="")
+OUTPUT_FILE <- "predition_probabilities_hc.tsv" # name your file
+write.table(pred.prob.hc, OUTPUT_FILE, sep="\t", row.names=F, quote=F, na="")
+synStore(File(OUTPUT_FILE, parentId=synapse.folder.id),
+         activityName = activityName,
+         activityDescription = activityDescription,
+         used = all.used.ids,
+         executed = thisFile)
+unlink(OUTPUT_FILE)
+
+# upload to Synapse, prediction probabilities
+synapse.folder.id <- parentId # synId of folder to upload your file to
+OUTPUT_FILE <- "predition_probabilities_record.tsv" # name your file
+write.table(pred.prob.record, OUTPUT_FILE, sep="\t", row.names=F, quote=F, na="")
 synStore(File(OUTPUT_FILE, parentId=synapse.folder.id),
          activityName = activityName,
          activityDescription = activityDescription,
